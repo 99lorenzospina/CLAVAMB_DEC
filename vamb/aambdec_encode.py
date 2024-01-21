7e1ad32f480e125f2c345a0dc0d4b4fc2db9a3dc
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-01-21 19:39:50 +0100

    changes to aambdec_encode

--- vamb/aambdec_encode.py
+++ vamb/aambdec_encode.py
@@ -15,10 +15,12 @@ import random
 import math
 
 from torch.utils.data import DataLoader as _DataLoader
+from torch.utils.data import Dataset
 from sklearn.cluster import KMeans
 
 from typing import Optional
 from argparse import Namespace
+import torch.nn.functional as F
 
 random_seed = 42
 torch.manual_seed(random_seed)
@@ -36,15 +38,10 @@ class AAEDEC(nn.Module):
         nsamples: int,
         nhiddens: int, #but it should be a list!
         nlatent_y: int, #careful: nlatent_y should be the number of estimated clusters
-        sl: float,
-        slr: float,
         lr: float,
         cri_lr: float,
         dis_lr: float,
-        alpha: Optional[float],
         _cuda: bool,
-        k: int = 4,
-        contrast: bool = False,
         optimizer_E = None,
         optimizer_D = None,
         optimizer_G = None,
@@ -60,20 +57,13 @@ class AAEDEC(nn.Module):
             )
 
         super(AAEDEC, self).__init__()
-        if alpha is None:
-            alpha = 0.15 if nsamples > 1 else 0.50
 
         self.nsamples = nsamples
         self.ntnf = ntnf
-        self.k = k
         self.h_n = nhiddens
         self.y_len = nlatent_y
         self.input_len = int(self.ntnf + self.nsamples)
-        self.sl = sl
-        self.slr = slr
-        self.alpha = alpha
         self.usecuda = _cuda
-        self.contrast = contrast
         self.lr = lr
         self.cri_lr = cri_lr
         self.dis_lr = dis_lr
@@ -133,6 +123,13 @@ class AAEDEC(nn.Module):
         torch.nn.init.xavier_normal_(self.cluster_layer.data)
         if optimizer_clusters == None:
             self.optimizer_clusters = torch.optim.Adam([self.cluster_layer], lr=self.lr)
+        else:
+            self.optimizer_clusters = optimizer_clusters
+        if optimizer_E != None:
+            self.optimizer_E = optimizer_E
+            self.optimizer_D = optimizer_D
+            self.optimizer_G = optimizer_G
+            self.optimizer_C = optimizer_C
 
         if _cuda:
             self.cuda()
@@ -203,42 +200,17 @@ class AAEDEC(nn.Module):
       except KeyError:
             ntnf = 103
       nsamples = dictionary['nsamples']
-      alpha = dictionary['alpha']
       nhiddens = dictionary['nhiddens']
-      nlatent_l = dictionary['nlatent_l']
       nlatent_y = dictionary['nlatent_y']
-      sl = dictionary['sl']
-      slr = dictionary['slr']
-      try:
-            k = dictionary['k']
-      except KeyError:
-            k = 4
-      try:
-            optimizer_E = dictionary["optimizer_E"]
-      except KeyError:
-            optimizer_E = None
-      try:
-            optimizer_D = dictionary["optimizer_D"]
-      except KeyError:
-            optimizer_D = None
-      try:
-            optimizer_D_z = dictionary["optimizer_D_z"]
-      except KeyError:
-            optimizer_D_z = None
-      try:
-            optimizer_D_y = dictionary["optimizer_D_y"]
-      except KeyError:
-            optimizer_D_y = None
-      try:
-            optimizer_awl = dictionary["optimizer_awl"]
-      except KeyError:
-            optimizer_awl = None
-
-
+      optimizer_E = dictionary["optimizer_E"]
+      optimizer_D = dictionary["optimizer_D"]
+      optimizer_G = dictionary["optimizer_G"]
+      optimizer_C = dictionary["optimizer_C"]
+      optimizer_clusters = dictionary["optimizer_clusters"]
       state = dictionary['state']
 
-      aae = cls(ntnf, nsamples, nhiddens, nlatent_l, nlatent_y, alpha, sl, slr, cuda, k=k, contrast=c, optimizer_E=optimizer_E,
-                optimizer_D=optimizer_D, optimizer_D_z=optimizer_D_z, optimizer_D_y=optimizer_D_y, optimizer_awl=optimizer_awl)
+      aae = cls(ntnf, nsamples, nhiddens, nlatent_y, cuda, optimizer_E=optimizer_E,
+                optimizer_D=optimizer_D, optimizer_G=optimizer_G, optimizer_C=optimizer_C, optimizer_clusters=optimizer_clusters)
       aae.load_state_dict(state)
 
       if cuda:
@@ -257,36 +229,19 @@ class AAEDEC(nn.Module):
         state = {
                  'ntnf': self.ntnf,
                  'nsamples': self.nsamples,
-                 'alpha': self.alpha,
                  'nhiddens': self.h_n,
-                 'nlatent_l': self.ld,
                  'nlatent_y': self.y_len,
-                 'sl' : self.sl,
-                 'slr': self.slr,
-                 'k' : self.k,
                  "optimizer_E": self.optimizer_E.state_dict(),
                  "optimizer_D": self.optimizer_D.state_dict(),
-                 "optimizer_D_z": self.optimizer_D_z.state_dict(),
-                 "optimizer_D_y": self.optimizer_D_y.state_dict(),
-                 **({"optimizer_awl": self.optimizer_awl.state_dict()} if self.optimizer_awl is not None else {}),
+                 "optimizer_G": self.optimizer_G.state_dict(),
+                 "optimizer_C": self.optimizer_D.state_dict(),
+                 "optimizer_clusters": self.optimizer_clusters.state_dict(),
+                 "n_enc_1": self.n_enc_1,
+                 "n_enc_2": self.n_enc_2,
                  'state': self.state_dict(),
                 }
 
         torch.save(state, filehandle)
-    
-    def calc_loss(self, depths_in, depths_out, tnf_in, tnf_out):
-        # If multiple samples, use cross entropy, else use SSE for abundance
-        if self.nsamples > 1:
-            # Add 1e-9 to depths_out to avoid numerical instability.
-            ce = -((depths_out + 1e-9).log() * depths_in).sum(dim=1).mean()
-            ce_weight = (1 - self.alpha) / log(self.nsamples)
-        else:
-            ce = (depths_out - depths_in).pow(2).sum(dim=1).mean()
-            ce_weight = 1 - self.alpha
-        sse = (tnf_out - tnf_in).pow(2).sum(dim=1).mean()
-        sse_weight = self.alpha / (tnf_in.shape[1] * 2)
-        loss = ce * ce_weight + sse * sse_weight
-        return loss, ce, sse
 
     def forward(self, depths_in, tnfs_in):
         mu = self._encode(depths_in, tnfs_in)
@@ -301,12 +256,11 @@ class AAEDEC(nn.Module):
             torch.pow(z.unsqueeze(1) - self.cluster_layer, 2), 2) / self.alpha)
         q = q.pow((self.alpha + 1.0) / 2.0)
         q = (q.t() / torch.sum(q, 1)).t()
-        return q, depths_out, tnfs_out
+        return q, z, depths_out, tnfs_out
 
     def pretrain(self, dataloader, max_iter, logfile):
         lr = self.lr
         cri_lr = self.cri_lr
-        Tensor = torch.cuda.FloatTensor if self.usecuda else torch.FloatTensor
         depthstensor, tnftensor = dataloader.dataset.tensors
         ncontigs, nsamples = depthstensor.shape
 
@@ -344,8 +298,8 @@ class AAEDEC(nn.Module):
             time_epoch_0 = time.time()
             self.train()
             (
-                crit_loss,
-                ed_loss,
+                CRIT_LOSS,
+                ED_LOSS,
             ) = (0,0)
 
             data_loader = _DataLoader(dataset=dataloader.dataset,
@@ -387,18 +341,27 @@ class AAEDEC(nn.Module):
                 crit_loss.backward()
                 self.optimizer_C.step()
 
-                ed_loss += ed_loss.item()
-                crit_loss += crit_loss.item()
+                ED_LOSS += ed_loss.item()
+                CRIT_LOSS += crit_loss.item()
                 
                 time_epoch_1 = time.time()
                 time_e = np.round((time_epoch_1 - time_epoch_0) / 60, 3)
+            
+            total_batches_inthis_epoch = len(data_loader)
+            if logfile is not None:
+                print(
+                    "\tEpoch: {}\t Loss Enc/Dec: {:.6f}\t Cri. loss: {:.4f}\t Batchsize: {}\t Epoch time(min): {: .4}".format(
+                          iter + 1,
+                          ED_LOSS / total_batches_inthis_epoch,
+                          CRIT_LOSS / total_batches_inthis_epoch,
+                          data_loader.batch_size,
+                          time_e,
+                    ), file=logfile)
+
+                logfile.flush()
 
         self.eval()
-        data = torch.Tensor(dataloader.dataset).to(device)
-        encoded,_,_ = self._encode(data, [])
-        kmeans = KMeans(n_clusters=self.y_len, n_init=20)
-        y_pred = kmeans.fit_predict(encoded.cpu().numpy())
-        return y_pred
+        return
 
     def discriminator_loss(real_output, fake_output, device):
         real_loss = torch.nn.BCEWithLogitsLoss(real_output, torch.ones_like(real_output, device=device))
@@ -406,29 +369,19 @@ class AAEDEC(nn.Module):
         total_loss = real_loss + fake_loss
         return total_loss
 
-    def calculate_kld(p, q):
-        # Ensure the arrays are probability distributions (sum to 1)
-        p = p / np.sum(p)
-        q = q / np.sum(q)
+
+    def student_t_distribution(self, q):
+        weight = q**2 / q.sum(0)
+        return (weight.t() / weight.sum(1)).t()
     
-        # Avoid division by zero by adding a small epsilon
-        epsilon = 1e-10
-    
-        # Calculate KLD
-        kld = np.sum(p * np.log((p + epsilon) / (q + epsilon)))
-
-        return kld
-
-
-    #STILL TO DO: deal with y_pred vector (all the contigs? How to do with batches?) and for the breakout option
-    def train(self, dataloader, max_iter, aux_iter, max_iter_dis, centers, C, targ_iter, tol, lrate,
-              logfile=None, modelfile=None, hparams=None, augmentationpath=None, mask=None):
+    def train(self, dataloader, max_iter, aux_iter, max_iter_dis, targ_iter, tol, lrate,
+              logfile=None):
         #C must be a torch.tensor of centroids (vectors), implicitly labeled by order
         
         Tensor = torch.cuda.FloatTensor if self.usecuda else torch.FloatTensor
         device = "cuda" if self.usecuda else "cpu"
-        depthstensor, tnftensor = dataloader.dataset.tensors
-        ncontigs, nsamples = depthstensor.shape
+        depthstensor, _ = dataloader.dataset.tensors
+        ncontigs, _ = depthstensor.shape
 
         # Pretrain discriminators
 
@@ -477,93 +430,105 @@ class AAEDEC(nn.Module):
 
                 time_epoch_1 = time.time()
                 time_e = np.round((time_epoch_1 - time_epoch_0) / 60, 3)
+            
+            total_batches_inthis_epoch = len(data_loader)
+            if logfile is not None:
+                print(
+                    "\tEpoch: {}\t Loss Discriminator: {:.6f}\t Batchsize: {}\t Epoch time(min): {: .4}".format(
+                          i + 1,
+                          G_loss / total_batches_inthis_epoch,
+                          data_loader.batch_size,
+                          time_e,
+                    ), file=logfile)
+
+                logfile.flush()
 
         #Clustering phase
 
+        class MyDataset(Dataset):
+            def __init__(self, data):
+                self.data = data
 
-        data_loader = _DataLoader(dataset=dataloader.dataset,
+            def __len__(self):
+                return len(self.data)
+
+            def __getitem__(self, idx):
+                depths_in, tnfs_in = self.data[idx]
+                return depths_in, tnfs_in, idx
+            
+        data_loader = _DataLoader(dataset=MyDataset(dataloader.dataset),
                                     batch_size=dataloader.batch_size,
                                     shuffle=True,
                                     drop_last=False,
                                     num_workers=dataloader.num_workers,
                                     pin_memory=dataloader.pin_memory)
+        
+        data = torch.Tensor(dataloader.dataset).to(device)
+        encoded,_,_ = self._encode(data, [])
+        kmeans = KMeans(n_clusters=self.y_len, n_init=20)
+        y_pred = kmeans.fit_predict(encoded.cpu().numpy())
+        y_pred_old = y_pred
+        self.cluster_layer.data = torch.tensor(kmeans.cluster_centers_).to(device)
+
         self.train()
         for epoch in range(max_iter):
             time_epoch_0 = time.time()
-            if epoch%targ_iter == 0:
-                
-        '''
-        for h in range(max_iter):
-            time_epoch_0 = time.time()
-            self.train()
-
             (D_loss,
              Cls_loss,
              G_loss,) = (0,0,0)
-            
-            Q = np.empty((0, self.y_len))
-            P = np.empty_like(Q)
-            y_pred = np.zeros(dataloader.batch_size)    #save the cluster for each sample
-            y_pred_old = y_pred
-            for depths_in, tnf_in in data_loader:   #take one batch
-                depths_in.requires_grad = True
-                tnf_in.requires_grad = True
-                if h%targ_iter == 0:
-                    mu = self._encode(depths_in, tnfs_in)
-                    for i in range(len(mu)):    #consider each sample separately, compute P and Q
-                        mu_i = mu[i]
-                        q_ij_values = np.empty(self.y_len)
-                        denominator = np.sum([self.student_t_distribution(mu_i, c) for _,c in self.optimizer_C.state_dict().items()])
-                        for _,c in self.optimizer_C.state_dict().items():
-                            numerator = self.student_t_distribution(mu_i, c)
-                            q_ij = numerator / denominator
-                            q_ij_values.append(q_ij)
-                        y_pred_old[i] = y_pred[i]
-                        y_pred[i] = np.argmax(q_ij_values)    #cluster assignment for sample i
-                        Q = np.vstack(Q, q_ij_values)
-                    for col in range(Q.shape[1]):
-                        P[:, col] = np.sum(Q[:, col])    #P contains the freq_qj, each column one frequence
-                    temp = np.empty_like(P)
-                    for i in range(len(mu)):
-                        for j in range(self.y_len):
-                            temp[i,j] = Q[i,j] ** 2 / P[i,j]
-                    for i in range(len(mu)):
-                        for j in range(self.y_len):
-                            P[i,j] = temp[i,j]/ np.sum(temp[i,:])
-                    #now P contains what I wanted
-                    del temp
-                    if (np.sum(y_pred != y_pred_old)< tol*dataloader.batch_size):    #cannot improve this batch anymore
-                        break
-                loss_g = self.discriminator_loss(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((detphs_out, tnfs_out), dim=1), device)
-                loss_d = torch.nn.MSEloss(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((detphs_out, tnfs_out), dim=1))
-                loss_cls = torch.nn.BCEWithLogitsLoss(torch.cat((detphs_out, tnfs_out), dim=1), torch.zeros_like(torch.cat((detphs_out, tnfs_out), dim=1), device=device))
-                loss_cls += self.kld(P,Q)
-                if (h % aux_iter <= (aux_iter/2)):
+            if epoch%targ_iter == 0:
+                #Compute q and p
+                tmp_q,_,_,_ = self.get_q(data)
+                tmp_q = tmp_q.data
+                p = self.student_t_distribution(tmp_q)
+                
+                #New assignment
+                y_pred = tmp_q.cpu().numpy().argmax(1)
+                delta_label = np.sum(y_pred != y_pred_old).astype(
+                    np.float32) / y_pred.shape[0]
+                y_pred_old = y_pred
+
+                if delta_label < tol:
+                    break   #cannot improve anymore
+            for batch, (depths_in, tnfs_in, idx) in data_loader:
+                q, _, depths_out, tnfs_out = self.get_q(depths_in, tnfs_in)
+                loss_g = self.discriminator_loss(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((depths_out, tnfs_out), dim=1), device)
+                loss_d = torch.nn.MSEloss(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((depths_out, tnfs_out), dim=1))
+                loss_cls = torch.nn.BCEWithLogitsLoss(torch.cat((depths_out, tnfs_out), dim=1), torch.zeros_like(torch.cat((depths_out, tnfs_out), dim=1), device=device))
+                loss_cls += F.kl_div(q.log(), p[idx])
+                D_loss += loss_d.item()
+                Cls_loss += loss_cls.item()
+                G_loss += loss_g.item()
+                if (epoch % aux_iter <= (aux_iter/2)):
                     self.optimizer_D.zero_grad()
                     loss_d.backward()
                     self.optimizer_D.step()                             
                 else:
                     self.optimizer_E.zero_grad()
+                    self.optimizer_clusters.zero_grad()
                     loss_cls.backward()
-                    C = C - self.lr * loss_cls.grad
                     self.optimizer_E.step() 
-                    self.optimizer_C.step()
+                    self.optimizer_clusters.step()
                     self.optimizer_D.zero_grad()
                     loss_d.backward()
                     self.optimizer_D.step()         
-                    optimizer_D_z.zero_grad()
+                    self.optimizer_G.zero_grad()
                     loss_g.backward()
-                    optimizer_D_z.step()
-                
-                        
-            time_epoch_1 = time.time()
-            time_e = np.round((time_epoch_1 - time_epoch_0) / 60, 3)
-        '''    
+                    self.optimizer_G.step()
+                time_epoch_1 = time.time()
+                time_e = np.round((time_epoch_1 - time_epoch_0) / 60, 3)
+            
+            total_batches_inthis_epoch = len(data_loader)
+            if logfile is not None:
+                print(
+                    "\tEpoch: {}\t Loss Enc: {:.6f}\t Loss Dec: {:.6f}\t Discriminator. loss: {:.4f}\t Batchsize: {}\t Epoch time(min): {: .4}".format(
+                          iter + 1,
+                          Cls_loss / total_batches_inthis_epoch,
+                          D_loss / total_batches_inthis_epoch,
+                          G_loss / total_batches_inthis_epoch,
+                          data_loader.batch_size,
+                          time_e,
+                    ), file=logfile)
+
+                logfile.flush()
         return
-    
-    def student_t_distribution(self, z, c):
-        # Calcola la distanza tra i vettori z_i e c_j
-        dist_squared = np.sum((z - c) ** 2)
-        # Calcola il numeratore della formula q_ij
-        numerator = (1 + dist_squared / self.degrees) ** (- (self.degrees + 1) / 2)
-        return numerator
