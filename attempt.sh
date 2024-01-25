fe49f6b77dfa9cf13bcfd84ad6697f71116bbdcd
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-01-25 14:36:35 +0100

    attempt.sh changed

--- attempt.sh
+++ attempt.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-#SBATCH -J airways_urog_skin__TNFabundance__nocontrastive__estimatek__noGPU__ONLYESTIM
-#SBATCH -o /nfsd/bcb/bcbg/spina/output_%j.txt
-#SBATCH -e /nfsd/bcb/bcbg/spina/errors_%j.txt
-#SBATCH -t 13:00:00
-#SBATCH -n 8
+#SBATCH -J airways__pcabundance__estimatek
+#SBATCH -o /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU/output_%j.txt
+#SBATCH -e /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU/errors_%j.txt
+#SBATCH -t 2:00:00
+#SBATCH -n 5
 #SBATCH -p allgroups
+#SBATCH --mem 13G
 #SBATCH -x runner-11
 
 cd /nfsd/bcb/bcbg/spina/
 source tesi_env/bin/activate
 cd CLAVAMB/vamb
 
-srun vamb --model aae --outdir /nfsd/bcb/bcbg/spina \
+srun vamb --model aae --outdir /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU \
 --fasta /nfsd/bcb/bcbg/spina/airways/contigs.fna.gz \
-/nfsd/bcb/bcbg/spina/skin/contigs.fna.gz \
-/nfsd/bcb/bcbg/spina/urog/contigs.fna.gz \
 --rpkm /nfsd/bcb/bcbg/spina/airways/abundance.npz  \
-/nfsd/bcb/bcbg/spina/skin/abundance.npz  \
-/nfsd/bcb/bcbg/spina/urog/abundance.npz  \
--o C
+--use_pc -o C
