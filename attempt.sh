ccf5d290e2cccdbbb24b041ff1769f8286263eb0
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-01-25 22:25:43 +0100

    debug

--- attempt.sh
+++ attempt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J airways__pcabundance__estimatek
-#SBATCH -o /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU/output_%j.txt
-#SBATCH -e /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU/errors_%j.txt
-#SBATCH -t 2:00:00
+#SBATCH -o /nfsd/bcb/bcbg/spina/estim_airways_pc/output_%j.txt
+#SBATCH -e /nfsd/bcb/bcbg/spina/estim_airways_pc/errors_%j.txt
+#SBATCH -t 01:00:00
 #SBATCH -n 5
 #SBATCH -p allgroups
 #SBATCH --mem 13G
@@ -12,7 +12,7 @@ cd /nfsd/bcb/bcbg/spina/
 source tesi_env/bin/activate
 cd CLAVAMB/vamb
 
-srun vamb --model aae --outdir /nfsd/bcb/bcbg/spina/airways__pcabundance__nocontrastive__estimatek__noGPU \
+srun vamb --model aae --outdir /nfsd/bcb/bcbg/spina/estim_airways_pc \
 --fasta /nfsd/bcb/bcbg/spina/airways/contigs.fna.gz \
 --rpkm /nfsd/bcb/bcbg/spina/airways/abundance.npz  \
 --use_pc -o C
