500cddc8ebff35b8cb1b4041ff0c689fbad13c95
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-02-04 20:06:21 +0100

    debugging

--- workflow_avamb/src/mv_bins_from_mdrep_clusters.py
+++ workflow_avamb/src/mv_bins_from_mdrep_clusters.py
@@ -59,13 +59,14 @@ Ritorna il sample di appartenenza del cluster
 def get_cluster_sample(
     cluster_contigs: dict[str, set[str]], bin_separator: Optional[str]
 ) -> dict[str, str]:
+    print("cluster_contigs", cluster_contigs)
     cluster_sample: dict[str, str] = dict()
-    print(cluster_sample)
     for cluster_ in cluster_contigs.keys():
         contigs = cluster_contigs[cluster_]
         contig_i = next(iter(contigs))
         sample = contig_i.split(bin_separator)[0]
         cluster_sample[cluster_] = sample
+    print("cluster_sample", cluster_sample)
     return cluster_sample
 
 
