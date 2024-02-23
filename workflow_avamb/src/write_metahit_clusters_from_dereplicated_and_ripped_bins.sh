9baefe32ad185faedfb6d8046266ea7b167014c5
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-02-23 19:56:16 +0100

    avamb

--- workflow_avamb/src/write_metahit_clusters_from_dereplicated_and_ripped_bins.sh
+++ workflow_avamb/src/write_metahit_clusters_from_dereplicated_and_ripped_bins.sh
@@ -9,9 +9,9 @@
 #SBATCH -x runner-11
 
 # Definizione dei percorsi dei bin finali e del file di output dei cluster
-drep_dir="/nfsd/bcb/bcbg/spina/pavamb_checkm_results/metahit/Final_bins/"
+drep_dir="/nfsd/bcb/bcbg/spina/avamb_checkm_results/metahit/Final_bins/"
 
-output_file="/nfsd/bcb/bcbg/spina/pavamb_checkm_results/metahit/pavamb_manual_drep_disjoint_clusters.tsv"
+output_file="/nfsd/bcb/bcbg/spina/avamb_checkm_results/metahit/avamb_manual_drep_disjoint_clusters.tsv"
 echo 'creating z y v clusters from the final set of bins'
 for s in $(ls $drep_dir)
 do
