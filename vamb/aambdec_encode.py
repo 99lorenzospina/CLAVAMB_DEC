ce225fbb90fa7d20cc5a4e725ca955a92b7576ed
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-02-29 18:49:31 +0100

    debugging

--- vamb/aambdec_encode.py
+++ vamb/aambdec_encode.py
@@ -358,7 +358,7 @@ class AAEDEC(nn.Module):
                 self.optimizer_D.zero_grad()
                 mu = self._encode(depths_in, tnfs_in)
                 depths_out, tnfs_out = self._decode(mu)
-                ed_loss = (torch.dist(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((depths_out, tnfs_out), dim=1), 2).pow(2)).sum(dim=0).mean() + s*torch.abs(self._critic(x))**2
+                ed_loss = (torch.dist(torch.cat((depths_in, tnfs_in), dim=1), torch.cat((depths_out, tnfs_out), dim=1), 2).pow(2)).sum(dim=0).mean() + s*torch.abs(self._critic(x)[0])**2
                 ed_loss.backward()                
                 self.optimizer_E.step()
                 self.optimizer_D.step()
@@ -367,7 +367,7 @@ class AAEDEC(nn.Module):
                 mu = self._encode(depths_in, tnfs_in)
                 depths_out, tnfs_out = self._decode(mu)
                 reg_term = self._critic(b*torch.cat((depths_in, tnfs_in), dim=1) + (1-b)*torch.cat((depths_out, tnfs_out), dim=1)).pow(2).sum(dim=1).mean()
-                crit_loss = torch.abs(self._critic(x) - factor1)**2 + reg_term
+                crit_loss = torch.abs(self._critic(x)[0] - factor1)**2 + reg_term
                 crit_loss.backward()
                 self.optimizer_C.step()
 
