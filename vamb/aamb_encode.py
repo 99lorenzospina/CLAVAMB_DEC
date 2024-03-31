d7e79260df771aa07f8b27a5e4fdf72f8b805836
99lorenzospina <lorenzo.spina@studenti.unipd.it>
2024-03-31 19:36:59 +0200

    changed seed

--- vamb/aamb_encode.py
+++ vamb/aamb_encode.py
@@ -27,12 +27,10 @@ import vamb.vambtools
 
 random_seed = 0
 torch.manual_seed(random_seed)
-'''
 torch.cuda.manual_seed(random_seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 np.random.seed(random_seed)
-'''
 
 ############################################################################# MODEL ###########################################################
 class AAE(nn.Module):
